79cd3668d3a7965d4bbf20a26c1ae347c574f7ec
jermuzet <jeremie.muz@gmail.com>
2023-05-22 13:56:55 +0000

    update create_U (closer to workflow syntax)

--- python_magnetworkflows/create_U.py
+++ python_magnetworkflows/create_U.py
@@ -6,31 +6,43 @@ from feelpp.toolboxes.core import *
 from feelpp.toolboxes.cfpdes import *
 
 import argparse
+import configparser
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--cfgfile", help="give cfg name", type=str)
-parser.add_argument("--odir", help="give output directory", type=str)
+parser.add_argument("cfgfile", help="input cfg file (ex. HL-31.cfg)")
 args = parser.parse_args()
 
-PWD = os.getcwd()
+pwd = os.getcwd()
+print(f"cwd: {pwd}")
+
+jsonfile = ""
+meshfile = ""
+feelpp_config = configparser.ConfigParser()
+with open(args.cfgfile, "r") as inputcfg:
+    feelpp_config.read_string("[DEFAULT]\n[main]\n" + inputcfg.read())
+
+    basedir = os.path.dirname(args.cfgfile)
+    print(f"basedir={basedir}")
+    if not basedir:
+        basedir = "."
+
+    jsonfile = feelpp_config["cfpdes"]["filename"]
+    jsonfile = jsonfile.replace(r"$cfgdir/", f"{basedir}/")
+    print(f"jsonfile={jsonfile}")
+
+    meshfile = feelpp_config["cfpdes"]["mesh.filename"]
+    meshfile = meshfile.replace(r"$cfgdir/", f"{basedir}/")
+    print(f"meshfile={meshfile}")
+
 app = feelpp.Environment(["myapp"], config=feelpp.localRepository(""))
 comm = feelpp.Environment.worldCommPtr()
-
-with open(args.cfgfile, "r") as fp:
-    for line in fp:
-        if line.startswith("filename="):
-            jsonfile = line.replace("filename=$cfgdir", PWD).rstrip()
-
-        if line.startswith("mesh.filename="):
-            meshfile = line.replace("mesh.filename=$cfgdir", PWD).rstrip()
-
 
 m2d = feelpp.load(feelpp.mesh(dim=2), name=meshfile, verbose=1)
 Xh = feelpp.functionSpace(space="Pch", mesh=m2d, order=1)
 usave = Xh.element()
 
 
-with open(jsonfile, "r") as file:
+with open(f"{pwd}/{jsonfile}", "r") as file:
     data = json.load(file)
     for param in data["Parameters"]:
         if param.startswith("U_"):
@@ -39,7 +51,5 @@ with open(jsonfile, "r") as file:
                 range=feelpp.markedelements(m2d, param[2:]), expr=feelpp.expr(f"{expr}")
             )
 
-usave.save(
-    args.odir,
-    name="U",
-)
+usave.save(f"{pwd}/{basedir}", name="U")
+print(f"odir={pwd}/{basedir}")
